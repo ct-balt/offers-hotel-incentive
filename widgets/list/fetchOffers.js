c04183a90cd1f0d2d8ad6077d0bf6731e284bf6e
ct-balt <evald.zuralovic@coraltravel.lt>
2025-08-25 09:31:15 +0300

    created fetching logic

--- widgets/list/fetchOffers.js
+++ widgets/list/fetchOffers.js
@@ -1,83 +1,48 @@
-function fetchOffers(offers) {}
+async function fetchOffers() {
+  const payload = generatePayloadPriceSearchEncrypt();
 
-const generatePayload = (offers) => {
-  const payload = {
-    searchCriterias: {
-      flightType: 2,
-      reservationType: 1,
-      beginDates: ["2025-08-23"],
-      datePickerMode: 0,
-      nights: [
-        {
-          value: 9,
-        },
-        {
-          value: 11,
-        },
-        {
-          value: 12,
-        },
-      ],
-      roomCriterias: [
-        {
-          passengers: [
-            {
-              passengerType: 0,
-              age: 20,
-            },
-            {
-              passengerType: 0,
-              age: 20,
-            },
-          ],
-        },
-      ],
-      departureLocations: [
-        {
-          id: 9239 - 5,
-          type: 5,
-          name: Vilnius,
-          friendlyUrl: vilnius,
-        },
-      ],
-      arrivalLocations: [
-        {
-          id: 5 - 3,
-          type: 3,
-          name: Antalija,
-          friendlyUrl: antalija,
-          parent: {
-            id: 1,
-            type: 0,
-            name: Turkija,
-            countryId: 1 - 0,
-          },
-        },
-      ],
-      paging: {
-        hasPreviousPage: false,
-        hasNextPage: false,
-        pageNumber: 1,
-        pageSize: 20,
-        sortType: 0,
+  showLoadingBanner();
+  hideWarningBanner();
+
+  try {
+    const priceSearchEncryptResponse = await callApi(
+      "https://www.coraltravel.lt/endpoints/PackageTourHotelProduct/PriceSearchEncrypt",
+      payload
+    );
+    const priceSearchPayload = generatePayloadPriceSearchList(
+      priceSearchEncryptResponse
+    );
+    const priceSearchListResponse = await callApi(
+      "https://www.coraltravel.lt/endpoints/PackageTourHotelProduct/PriceSearchList",
+      priceSearchPayload
+    );
+
+    listOffers(priceSearchListResponse);
+  } catch (error) {
+    showWarningBanner();
+    console.error("error in fetchoffers", error);
+  } finally {
+    hideLoadingBanner();
+  }
+}
+
+async function callApi(apiUrl, payload) {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/plain, */*",
       },
-      imageSizes: [4],
-      additionalFilters: [
-        {
-          type: 2,
-          values: [
-            {
-              id: 4,
-              value: 4,
-            },
-          ],
-          providers: [],
-        },
-      ],
-    },
-    notIncludeFilters: false,
-    searchSource: 2,
-    getOnlyTopHotels: false,
-    dontSearchTopHotels: true,
-  };
-};
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
